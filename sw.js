ffaf7af602164adba13bb1865a33c0e97b66d5b3
danktrust15-del <danktrust15@gmail.com>
2026-09-22 01:14:08 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'dankconnect-v31';
+const CACHE_NAME = 'dankconnect-v32';
 const ASSETS = [
   './',
-  './index.html?v=31',
+  './index.html?v=32',
   './index.html',
-  './manifest.json?v=31',
+  './manifest.json?v=32',
   './manifest.json',
-  './logo.png?v=31',
+  './logo.png?v=32',
   './logo.png'
 ];
 
 self.addEventListener('install', (e) => {
-  console.log('[SW V31] Install');
+  console.log('[SW V32] Install - 50 msg cache build');
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(err=>console.log('cache fail',err))));
 });
 
 self.addEventListener('activate', (e) => {
-  console.log('[SW V31] Activate');
+  console.log('[SW V32] Activate - deleting old caches');
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
   );
@@ -34,7 +34,7 @@ self.addEventListener('fetch', (e) => {
         const copy = res.clone();
         caches.open(CACHE_NAME).then(c=>c.put(req, copy));
         return res;
-      }).catch(() => caches.match('./index.html') || caches.match('./index.html?v=31') || caches.match(req))
+      }).catch(() => caches.match('./index.html?v=32') || caches.match('./index.html') || caches.match(req))
     );
     return;
   }
